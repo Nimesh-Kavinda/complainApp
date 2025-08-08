53b92fee40c62b30c1f7aa0ac55b3ff3494755e5
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-08-08 21:59:55 +0530

    Remove some unneassry Links

--- resources/views/layouts/departmentHead.blade.php
+++ resources/views/layouts/departmentHead.blade.php
@@ -71,24 +71,6 @@
                         <span class="ml-auto bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200 text-xs px-2 py-1 rounded-full">42</span>
                     </a>
                 </li>
-                <li>
-                    <a href="#" class="nav-link flex items-center px-4 py-3 text-gray-700 dark:text-gray-300 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
-                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z"></path>
-                        </svg>
-                        Staff Members
-                        <span class="ml-auto bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200 text-xs px-2 py-1 rounded-full">10</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#"class="nav-link flex items-center px-4 py-3 text-gray-700 dark:text-gray-300 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
-                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
-                        </svg>
-                        Requests
-                        <span class="ml-auto bg-yellow-100 text-blue-800 dark:bg-yellow-600 dark:text-blue-200 text-xs px-2 py-1 rounded-full">5</span>
-                    </a>
-                </li>
             </ul>
         </nav>
 
